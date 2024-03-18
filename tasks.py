090de4ce04aa49c7fdc0a8dbeded9971f5e75f9f
john <xxxzevavolkxxx@gmail.com>
2024-03-19 00:54:02 +0300

    task 4

--- tasks.py
+++ tasks.py
@@ -1,4 +1,9 @@
-def take():
-    print("It's my first function")
+def take(x):
+    v_max = max(x)
+    v_min = min(x)
+    v_sum = sum(x)
+    print(f"Min = {v_min}, max = {v_max}, sum = {v_sum}")
 
-take()
+x = input().split()
+f = [int(y) for y in x]
+take(f)
